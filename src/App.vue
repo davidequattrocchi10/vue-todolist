8394c80793ae31648ea8a621d63bcde53f9f2d5f
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-02-07 16:58:50 +0100

    Done Milestone 3 plus bonus with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ export default {
   name: "App",
   data() {
     return {
+      new_todo: "",
+      error: "",
+      to_do: false,
       tasks: [
         {
           text: "Wake up",
@@ -37,28 +40,53 @@ export default {
   },
   methods: {
     eliminateTask(index) {
-      this.tasks.splice(index, 1)
+      this.tasks.splice(index, 1);
+    },
+    addTask(text, done) {
+      if (this.new_todo.length > 4) {
+        this.error = "";
+        this.tasks.push({ text, done });
+        this.new_todo = "";
+      } else {
+        this.error = "The task must have al least 5 letters";
+
+      }
+    },
+    changeDone(value, index) {
+      if (value == true) {
+        this.tasks[index].done = false
+      } else {
+        this.tasks[index].done = true
+      }
     }
   }
-
 }
 
 </script>
 
 <template>
-  <h1>Todo List</h1>
   <div class="container">
-    <ul v-if="tasks.length != 0">
-      <li v-for="(task, index) in tasks">
-        <div v-if="task.done" class="barred">
-          {{ task.text }} <button @click="eliminateTask(index)"> X </button></div>
-        <div v-else class="no_barred">
-          {{ task.text }} <button @click="eliminateTask(index)"> X </button> </div>
+    <div class="card bg-light mb-3">
+      <h1 class="card-header">Todo list</h1>
+      <div class="card-body">
+        <div class="p-2"> <input v-model="new_todo" @keyup.enter="addTask(new_todo, to_do)"> <button
+            class="btn btn-primary" @click="addTask(new_todo, to_do)">
+            Add
+          </button> </div>
+        <p style="color: red;"> {{ error }}</p>
+        <ul class="list-group" v-if="tasks.length > 0">
+          <li class="list-group-item" v-for="(task, index) in tasks">
+            <div class="d-flex justify-content-between align-items-center">
+              <span :class="{ barred: task.done }" @click="changeDone(task.done, index)">{{ task.text }} </span> <span>
+                <button class="btn btn-danger" @click="eliminateTask(index)"> X </button></span>
+            </div>
 
 
-      </li>
-    </ul>
-    <ul v-else style="font-size: larger; color: blue; font-weight: bold;">There aren't things to do</ul>
+          </li>
+        </ul>
+        <ul v-else style="font-size: larger; color: blue; font-weight: bold;">There aren't things to do</ul>
+      </div>
+    </div>
   </div>
 </template>
 
